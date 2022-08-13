bd73524896932d4c668854e80edb1c38f084c8e6
mdwisu <dwisusanto784@gmail.com>
2022-08-13 12:56:02 +0700

    melanjutkan pembalajaran sebelumnya

---
+++ 18-express-contact-2/app.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const { loadContact, findContact } = require('./utils/contacts');
+
+const app = express();
+const port = 3000;
+
+// gunakan ejs
+app.set('view engine', 'ejs');
+
+// third-party middleware
+app.use(expressLayouts);
+
+// built in middleware
+app.use(express.static('public'));
+
+// application level middleware
+app.use((req, res, next) => {
+  console.log('Time', Date.now());
+  next();
+});
+
+// ! home
+app.get('/', (req, res) => {
+  // res.sendFile('./index.html', { root: __dirname });
+  const mahasiswa = [
+    {
+      nama: 'dwisusanto',
+      email: 'dwisusanto784@gmail.com',
+    },
+    {
+      nama: 'intanpermatasari',
+      email: 'intan@gmail.com',
+    },
+    {
+      nama: 'shandikagalih',
+      email: 'shandika@gmail.com',
+    },
+  ];
+  res.render('index', {
+    layout: 'layouts/main-layout',
+    nama: 'Muhammad Dwi Susanto',
+    title: 'Home',
+    mahasiswa,
+  });
+});
+
+// ! about
+app.get('/about', (req, res) => {
+  res.render('about', {
+    layout: 'layouts/main-layout',
+    title: 'halaman about',
+  });
+});
+
+// ! contact
+app.get('/contact', (req, res) => {
+  const contacts = loadContact();
+  res.render('contact', {
+    layout: 'layouts/main-layout',
+    title: 'halaman contact',
+    contacts
+  });
+});
+app.get('/contact/:nama', (req, res) => {
+  const contact = findContact(req.params.nama);
+  res.render('detail', {
+    layout: 'layouts/main-layout',
+    title: 'halaman detail contact',
+    contact
+  });
+});
+
+app.use((req, res) => {
+  res.status(404);
+  res.send('<h1>404</h1>');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
